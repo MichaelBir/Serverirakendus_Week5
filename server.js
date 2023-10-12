fb7e0cfc5623722050106566586b8e01102b6ad5
michaelbir <mikux22@gmail.com>
2023-10-12 20:20:12 +0300

    fisrst commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');  
-const port = 80;
+const port = 3000;
 
 const server = http.createServer((request, response) => {
     const requestUrl = url.parse(request.url).pathname;
